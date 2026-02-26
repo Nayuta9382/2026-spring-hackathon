46ca59043bfc9f3fef6c8f56924a83f7bb4a742e
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2026-02-26 15:36:42 +0900

    modelファイルの作成

--- schedules/models.py
+++ schedules/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# スケージュール
+class Schedule(models.Model):
+    event = models.ForeignKey('events.Event', on_delete=models.CASCADE, related_name='schedules')
+    detail = models.CharField(max_length=255) # スケージュール詳細
+    result = models.CharField(max_length=255,blank=True) # 結果
+    status = models.PositiveIntegerField(default = 0) # 進捗ステータス
+    order = models.PositiveIntegerField(default = 0) # 順序
+
+    class Meta:
+        # 順序が低い順（1, 2, 2, 3...）に並べる
+        ordering = ['order']
+        # 「event」と「order」のペアで重複を禁止する
+        unique_together = ('event', 'order')
+
+    def __str__(self):
+        return f"{self.event.name} - {self.detail} (順序: {self.order})"
--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# チームグループ
+class TeamGroup(models.Model):
+    tournament = models.ForeignKey('tournaments.Tournament', on_delete=models.CASCADE, related_name='team_groups')
+    category = models.PositiveIntegerField(default = 0) # チームステータス区分
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.tournament.name} - {self.category}"
+
+
+# チーム
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    team_group = models.ForeignKey(TeamGroup, on_delete=models.CASCADE, related_name='team')
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
