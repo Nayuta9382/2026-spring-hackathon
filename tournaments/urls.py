6df8b137e4dafa2da5c42408981a2534bafde373
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2026-02-27 14:39:23 +0900

    大会一覧機能の作成 #10

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TournamentCreateView
+from .views import TournamentCreateView, TournamentListView
 
 urlpatterns = [
     # path(URLの文字列, ビュー, 名前)
     path('create/', TournamentCreateView.as_view(), name='create'),
+    path('', TournamentListView.as_view(), name='list'),
 ]
