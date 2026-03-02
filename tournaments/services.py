185eb64a74806e85cdbf03530f22639e987dc2ff
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2026-03-02 09:55:47 +0900

    大会詳細ページのデータの表示機能の実装 #13

--- tournaments/services.py
+++ tournaments/services.py
@@ -1,12 +1,11 @@
-from django.db import transaction
+from django.db import transaction,connection
 from .models import Tournament, TournamentPoint
 from teams.models import TeamGroup, Team
+from django.shortcuts import get_object_or_404
 
-# 大会作成機能のサービスクラス
+# 大会作成機能のサービス関数
 def create_tournament_with_teams(tournament_data):
     with transaction.atomic():
-
-        
         # 1. 大会作成
         tournament = Tournament.objects.create(
            name=tournament_data.get('name'), 
@@ -31,3 +30,41 @@ def create_tournament_with_teams(tournament_data):
         ])
     
     return tournament
+
+# 大会idから詳細情報を取得する
+def get_tournament_detail(id):
+    tournament = get_object_or_404(Tournament, pk=id)
+
+    return tournament
+
+
+# 大会idからその大会のクラスの総合順位を取得する
+def get_tournament_rannkings(tournament_id):
+    query = """
+        SELECT 
+            t.name AS name,
+            SUM(er.point) AS total_point,
+            RANK() OVER (ORDER BY SUM(er.point) DESC) AS rank
+        FROM teams_teamgroup tg
+        INNER JOIN teams_team t 
+            ON tg.id = t.team_group_id
+        INNER JOIN events_event e 
+            ON tg.id = e.team_group_id
+        INNER JOIN event_results_eventresult er 
+            ON e.id = er.event_id
+        WHERE 
+            tg.tournament_id = %s
+        GROUP BY 
+            t.id, t.name
+        ORDER BY 
+            rank;
+    """
+
+    with connection.cursor() as cursor:
+        cursor.execute(query, [tournament_id])
+        
+        # 全行取得して辞書のリストにする
+        columns = [col[0] for col in cursor.description]
+        rankings = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    return rankings
