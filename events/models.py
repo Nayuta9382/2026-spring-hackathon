fec5ffb1d8dda9f590bdbb7afbb479dabb6c1661
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2026-03-04 15:04:26 +0900

    競技作成機能の修正 #15

--- events/models.py
+++ events/models.py
@@ -6,7 +6,7 @@ class Event(models.Model):
     name = models.CharField(max_length=255) # 競技名
     category = models.PositiveIntegerField(default=0) # 競技区分
     team_group = models.ForeignKey('teams.TeamGroup', on_delete=models.CASCADE, related_name='team_events') # チームグループID
-    detail = models.TextField(max_length=2000, default='') # 競技詳細
+    detail = models.TextField(max_length=2000, default='',blank=True) # 競技詳細
     tournament_img = models.ImageField(upload_to='tournaments/', null=True, blank=True) #トーナメント画像
     event_img = models.ImageField(upload_to='events/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
