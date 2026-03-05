d4758f5f3c4cd355d718e4ec2c243640c974d35b
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2026-03-05 11:43:24 +0900

    管理者競技詳細にチーム一覧を表示 #38

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # 大会のID（tournament_pk）を指定して競技を作成するURL
     path('add', EventCreateView.as_view(), name='event_create'),
     # 競技詳細
-    path('<int:pk>', EventAdminDetailView.as_view(), name='event_detail_admin'),
+    path('<int:pk>/admin', EventAdminDetailView.as_view(), name='event_detail_admin'),
     # 競技を編集する
     path('<int:pk>/edit', EventEditView.as_view(), name='event_edit'),
 
