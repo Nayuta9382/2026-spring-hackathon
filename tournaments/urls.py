c4e9d7915316f000ad07c8a5302e0afc4d32f505
out-in <y.tonouchi.sys23@morijyobi.ac.jp>
2026-03-05 10:23:01 +0900

    スケジュール編集

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TournamentCreateView, TournamentListView,TournamentDetailView,TournamentUpdateView
+from .views import TournamentCreateView, TournamentListView, TournamentDetailView, TournamentUpdateView
 from .views import UpdateStatusView
 from .views import TournamentDeleteView
+from schedules.views import ScheduleUpdateView, ScheduleListView
 
 urlpatterns = [
     # 大会一覧
@@ -12,8 +13,12 @@ urlpatterns = [
     path('<int:pk>/admin', TournamentDetailView.as_view(), name='tournament_detail_admin'),
     # 管理者大会編集
     path('<int:pk>/edit', TournamentUpdateView.as_view(), name='tournament_update'),
-    # 大会ステータスのpulldwon
+    # 大会ステータスのpulldown
     path('<int:pk>/api/update-status/', UpdateStatusView.as_view(), name='api_update_status'),
     # 大会削除
     path('<int:pk>/delete', TournamentDeleteView.as_view(), name='tournament_delete'),
+    # スケジュール一覧（next/now/previous表示）
+    path('schedules/<int:pk>/', ScheduleListView.as_view(), name='schedule_list'),
+    # スケジュール編集
+    path('schedules/<int:pk>/edit/', ScheduleUpdateView.as_view(), name='schedule_edit'),
 ]
