463c2654a4629b0f0f15d8686de1c3cf2297c745
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2026-03-09 16:00:12 +0900

    メニューをスーパユーザでは表示しないように制御 #111

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -9,10 +9,6 @@ from django.utils.http import urlencode
 def admin_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-
-
-
-
         # スーパユーザとしてログインしているかを認証
         if is_superuser_authenticated(request):
             # 認証されていれば、そのままビューを実行
@@ -37,8 +33,6 @@ def admin_required(view_func):
 def super_user_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-
-    
         # スーパユーザとしてログインしているかを認証
         if is_superuser_authenticated(request):
             # 認証されていれば、そのままビューを実行
@@ -51,8 +45,6 @@ def super_user_required(view_func):
 def operator_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-
-
         # 運営者とログインしているかを認証
         if is_operator_authenticated(request):
             return view_func(request, *args, **kwargs)
